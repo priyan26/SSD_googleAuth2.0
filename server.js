7b433310300679517b2b4a2b6fd465723cb83863
IT18094800 <it18094800@my.sliit.lk>
2021-09-22 22:33:40 +0530

    Implemented truncate and striptag helpers

--- server.js
+++ server.js
@@ -36,10 +36,20 @@ app.use(session({
 }))
 
 //Handlebars Helpers
-const {formatDate} = require('./helpers/hbs')
+const {formatDate, stripTags, truncate} = require('./helpers/hbs')
 
 // Handlebars
-app.engine('.hbs', exphbs({helpers:{formatDate,}, defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs',
+    exphbs({
+        helpers:{
+            formatDate,
+            stripTags,
+            truncate,
+        },
+        defaultLayout: 'main',
+        extname: '.hbs'
+    })
+);
 app.set('view engine', '.hbs');
 
 //Set Passport Middleware

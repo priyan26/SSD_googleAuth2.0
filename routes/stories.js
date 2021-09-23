cccb8f9d4bc831a30f9a17d3a09de5dbddd0dcbe
IT18094800 <it18094800@my.sliit.lk>
2021-09-23 15:00:42 +0530

    Implemented method to edit stories

--- routes/stories.js
+++ routes/stories.js
@@ -56,6 +56,27 @@ router.get('/edit/:id', ensureAuth, async (req,res)=>{
            poem,
        })
    }
-
 })
+
+//@desc Update story
+//@route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req,res)=>{
+    let story = await Poem.findById(req.params.id).lean()
+
+    if (!story){
+        return res.render('error/404')
+    }
+
+    if(story.user!=req.user.id){
+        res.redirect('/stories')
+    }else{
+        story = await Poem.findOneAndUpdate({_id: req.params.id}, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        res.redirect('/dashboard')
+    }
+})
+
 module.exports=router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session')
 const passport = require('passport')
 const exphbs = require('express-handlebars')
 const MongoStore = require('connect-mongo')
+const methodOverride = require('method-override')
 
 // Load the Globals
 dotenv.config({path: './Globals/.env'})
@@ -19,6 +20,16 @@ const app = express()
 //Body parser
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+//Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
